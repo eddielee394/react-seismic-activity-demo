a18fbe05d204f5232729b2c02b3364f5a3a6aa54
Eddie Padin <eddie@entertainermedia.net>
2019-09-30 07:38:49 -0500

    init architecture; - refactor mock data; - add delay to mock api requests to initialize loader;

--- src/data/db/locationData.js
+++ src/data/db/locationData.js
@@ -1,141 +1,126 @@
 import mock from "../mock";
 
-const locationData = [
+const locations = [
   {
     id: 1,
-    location: "a",
+    name: "a",
     y: 36.80638477380125,
     x: 39.4449153947703,
-    magnitude: 5.8,
-    depth: 4
+    magnitude: 5.8
   },
 
   {
     id: 2,
-    location: "c",
+    name: "c",
     y: 92.72378114627274,
     x: 85.19150475473184,
-    magnitude: 8.0,
-    depth: 2
+    magnitude: 8.0
   },
 
   {
     id: 3,
-    location: "b",
+    name: "b",
     y: 6.307027617647912,
     x: 65.97939639324936,
-    magnitude: 9.0,
-    depth: 4
+    magnitude: 9.0
   },
 
   {
     id: 4,
-    location: "a",
+    name: "a",
     y: 37.45016534487874,
     x: 38.49627026221381,
-    magnitude: 1.0,
-    depth: 1
+    magnitude: 1.0
   },
 
   {
     id: 5,
-    location: "d",
+    name: "d",
     y: 15.81937888166879,
     x: 30.107143522811732,
-    magnitude: 3.4,
-    depth: 1
+    magnitude: 3.4
   },
 
   {
     id: 6,
-    location: "b",
+    name: "b",
     y: 4.697501854713504,
     x: 65.12057699689227,
-    magnitude: 3.3,
-    depth: 1
+    magnitude: 3.3
   },
 
   {
     id: 7,
-    location: "f",
+    name: "f",
     y: 49.72539249123659,
     x: 67.9858657771189,
-    magnitude: 3.7,
-    depth: 1
+    magnitude: 3.7
   },
 
   {
     id: 8,
-    location: "f",
+    name: "f",
     y: 49.31328972292483,
     x: 68.09402623068067,
-    magnitude: 1.1,
-    depth: 2
+    magnitude: 1.1
   },
 
   {
     id: 9,
-    location: "e",
+    name: "e",
     y: 38.18290046154109,
     x: 81.71360463623895,
-    magnitude: 2.1,
-    depth: 2
+    magnitude: 2.1
   },
 
   {
     id: 10,
-    location: "f",
+    name: "f",
     y: 49.79353997764336,
     x: 68.01373401680416,
-    magnitude: 1.4,
-    depth: 5
+    magnitude: 1.4
   },
 
   {
     id: 11,
-    location: "d",
+    name: "d",
     y: 15.556831287795688,
     x: 30.77308955210717,
-    magnitude: 3.4,
-    depth: 2
+    magnitude: 3.4
   },
 
   {
     id: 12,
-    location: "d",
+    name: "d",
     y: 15.887178087020478,
     x: 30.756777903530697,
-    magnitude: 4.6,
-    depth: 4
+    magnitude: 4.6
   },
 
   {
     id: 13,
-    location: "g",
+    name: "g",
     y: 4.759693693983893,
     x: 48.78450141787685,
-    magnitude: 5.4,
-    depth: 1
+    magnitude: 5.4
   },
 
   {
     id: 14,
-    location: "c",
+    name: "c",
     y: 91.20786702227447,
     x: 86.86112435683953,
-    magnitude: 7.1,
-    depth: 1
+    magnitude: 7.1
   },
 
   {
     id: 15,
-    location: "d",
+    name: "d",
     y: 16.706632368872377,
     x: 30.57634811456861,
-    magnitude: 2.0,
-    depth: 1
+    magnitude: 2.0
   }
 ];
 mock.onGet("/api/locations").reply(config => {
-  return [200, locationData];
+  return [200, locations];
 });
--- src/data/mock.js
+++ src/data/mock.js
@@ -5,5 +5,5 @@ import MockAdapter from "axios-mock-adapter";
  * Instantiates a new MockAdapter instance
  * @type {MockAdapter}
  */
-const mock = new MockAdapter(axios);
+const mock = new MockAdapter(axios, { delayResponse: 750 });
 export default mock;
