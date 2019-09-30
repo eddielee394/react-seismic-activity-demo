2d5c98749186733ae28e1a9b2923d44b054237c4
Eddie Padin <eddie@entertainermedia.net>
2019-09-30 07:37:32 -0500

    init architecture; - update css; - add tailwind build scripts; - add react-vis, jest, jss, lodash & mui-datatables support;

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import logo from "../assets/images/logo.svg";
-import "../assets/styles/App.css";
-import LocationList from "./components/Location/LocationList";
 import "../data";
+import "../assets/styles/index.css";
+import LocationContainer from "./components/Location/LocationContainer";
 
 const App = () => {
   return (
-    <div className="App">
-      <LocationList />
+    <div>
+      <LocationContainer />
     </div>
   );
 };
