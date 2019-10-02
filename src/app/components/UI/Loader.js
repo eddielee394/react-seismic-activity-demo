dd536901c1e38a20c30dc7c23c925ac9124ee79d
Eddie Padin <eddie@entertainermedia.net>
2019-10-01 19:26:54 -0500

    unit testing; - add deps: react-testing-library/jest-dom - add testid attribute to loader; - add unit tests for LocationContainer, Api & main app rendering;

--- src/app/components/UI/Loader.js
+++ src/app/components/UI/Loader.js
@@ -5,7 +5,10 @@ const Loader = props => {
   const { isLoading } = props;
   return (
     <div className="container flex flex-col flex-grow mx-auto px-8 sm:px-16 py-24">
-      <div className="flex justify-center align-middle my-128">
+      <div
+        className="flex justify-center align-middle my-128 loader-container"
+        data-testid="loader-container"
+      >
         <BeatLoader color={"#123abc"} loading={isLoading} />
       </div>
     </div>
